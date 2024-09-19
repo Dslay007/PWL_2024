c455c3ac78d2f538bd3bb9eadeda2a9d2fb154b3
Dslay007 <donaharyanto007@gmail.com>
2024-09-20 00:03:14 +0700

    Jobsheet 4 Praktikum 2.3

--- PWL_POS/resources/views/user.blade.php
+++ PWL_POS/resources/views/user.blade.php
@@ -1,25 +1,17 @@
-
 <!DOCTYPE html>
-    <html>
-    <title>Data User</title>
+<html>
     <head>
-        <h1>Data User</h1>
+        <title>Data User</title>
     </head>
-                <body>
-                
-                <table border="1" cellpadding="2" cellspacing="0">
-                <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th>
-                <th>ID Level Pengguna</th>
-                </tr>
-                    <tr>
-                        <td>{{ $data->user_id }}</td>
-                        <td>{{ $data->username }}</td> 
-                        <td>{{ $data->nama }}</td>
-                        <td>{{ $data->level_id }}</td>
-                        </tr>
-                </table>
-                </body>
-    </html>
+    <body>
+        <h1>Data User</h1>
+        <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <th>Jumlah Pengguna</th>
+            </tr>
+            <tr>
+                <td>{{ $userCount }}</td>
+            </tr>
+        </table>
+    </body>
+</html>
