6e17ed21b50e6b876692d000375e07a65449db1e
Dslay007 <donaharyanto007@gmail.com>
2024-09-30 10:59:11 +0700

    Jobsheet 5 Praktikum 3

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -17,6 +17,18 @@ use App\Http\Controllers\KategoriController;
 
  Route::get('/', [WelcomeController::class, 'index']); 
 
+ 
+Route::group(['prefix' => 'user'], function () {
+Route::get('/', [UserController::class, 'index']); 
+Route::post('/list', [UserController::class, 'list']); 
+Route::get('/create', [UserController::class, 'create']); 
+Route::post('/', [UserController::class, 'store']); 
+Route::get('/{id}', [UserController::class, 'show']); 
+Route::get('/{id}/edit', [UserController::class, 'edit']); 
+Route::put('/{id}', [UserController::class, 'update']); 
+Route::delete('/{id}', [UserController::class, 'destroy']); 
+});
+
 // Route::get('/', function () { return view('welcome');});
 // Route::get('/level', [LevelController::class, 'index']); 
 // Route::get('/kategori', [KategoriController::class, 'index']);
