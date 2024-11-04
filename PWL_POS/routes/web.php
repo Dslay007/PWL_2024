2a4db2ae69d16a77beff97b5c82a31fe921c7472
Dslay007 <donaharyanto007@gmail.com>
2024-11-04 08:40:07 +0700

    Jobsheet 10 Praktikum 4

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
